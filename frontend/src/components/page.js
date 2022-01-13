ddf8fe44ec2e20c4591ab8be0245874242967717
Jabez Borja <jabez.natsu@gmail.com>
2022-01-13 23:57:03 +0800

    Tweak: Minor page design

--- frontend/src/components/page.js
+++ frontend/src/components/page.js
@@ -21,11 +21,11 @@ export default function Page () {
 
 const MainPage = ({ user }) => {
     return (user.username ? (
-        <main>
+        <main className='pb-7 shadow-2xl'>
             <div>
                 {user.cover
-                    ? <img className='absolute object-cover -z-10 top-0.5 w-screen h-4/6' src={user.cover}></img>
-                    : <div className='absolute object-cover -z-10 top-0.5 w-screen h-4/6 bg-[#181717]'></div>}
+                    ? <img className='absolute object-cover -z-10 top-0 w-screen h-4/6' src={user.cover}></img>
+                    : <div className='absolute object-cover -z-10 top-0 w-screen h-4/6 bg-[#181717]'></div>}
             </div>
             <div className="flex flex-col justify-center items-center m-auto mt-8 content-center bg-[#222222] rounded-md py-20 shadow w-10/12 sm:w-10/12 lg:w-8/12 xl:w-6/12">
                 <img
@@ -41,7 +41,7 @@ const MainPage = ({ user }) => {
                         ? <span><img className='w-10/12' src={CheckMark}></img></span>
                         : <div/>}
                 </div>
-                <div className="flex flex-row mt-5 space-x-3">
+                <div className="flex flex-wrap px-4 mt-5 mx-32">
                     {Object.entries(user.game_links)
                         .map(([game_name, values], i) => (
                             <GameLink key={i} game_name={game_name} game_link={values.link} />
@@ -57,7 +57,7 @@ const MainPage = ({ user }) => {
 
 const GameLink = ({ game_name, game_image, game_link }) => {
     return (
-        <a href="#" onClick={() => openLink(game_link)} className="text-white border-[0.7px] border-[#898989] rounded-full py-2 px-6">
+        <a href="#" onClick={() => openLink(game_link)} className="text-white border-[0.7px] border-[#898989] rounded-full py-2 px-3 mx-1 mt-3 m-auto">
             <div className="flex flex-row space-x-2">
                 <h1>{game_name}</h1>
                 <img className="w-3" src={ExternalLink}></img>
