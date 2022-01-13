da7f4d43b9aae08d5f3c2205b15f17beccb87bf7
Jabez Borja <jabez.natsu@gmail.com>
2022-01-13 15:38:54 +0800

    Tweak: Page design

--- frontend/src/components/page.js
+++ frontend/src/components/page.js
@@ -14,12 +14,38 @@ export default function Page () {
         dispatch(fetchUser(userslug))
     }, [user, dispatch])
 
+    return (<MainPage user={user} game_data={game_data}/>);
+}
+
+function MainPage({ user, game_data }) {
     return (
-        <div className="text-white">
-            Username: {user.username}#{user.usertag}<br></br>
-            Userslug: {user.userslug}<br></br>
-            Bio: {user.bio}<br></br>
-            Current Playing: {user.current_playing}<br></br>
-        </div>
-    )
+        <main className="flex flex-col justify-center items-center m-auto mt-8 content-center bg-[#222222] rounded-md py-20 shado w-10/12 sm:w-10/12 lg:w-8/12 xl:w-6/12">
+            <img
+                className="w-32 rounded-full"
+                src={user.pfp}
+            ></img>
+            <div className="mt-5 text-3xl text-white">
+                <span className="font-bold">{user.username}</span>
+                <span className="text-[#878787]">#{user.usertag}</span>
+            </div>
+            <div className="flex flex-row mt-5 space-x-3">
+                {user.game_links.map((game, i) => <GameLink key={i} game_name={game} />)}
+            </div>
+            <p className="mt-6 text-white">
+                {user.bio}
+            </p>
+        </main>
+    );
+}
+
+function GameLink({ game_name, game_image }) {
+    return (
+        <button className="text-white border-[0.7px] border-[#898989] rounded-full py-1 px-4">
+            <div>
+                <img src={game_image}></img>
+                <h1>{game_name}</h1>
+                <img></img>
+            </div>
+        </button>
+    );
 }
