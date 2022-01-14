8847188e92c350cbd1302e37624ef634eb415896
Jabez Borja <jabez.natsu@gmail.com>
2022-01-15 04:57:13 +0800

    fix: api endpoint url error

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const APIENDPOINT = "https://https://calm-spire-77666.herokuapp.com/api/";
+const APIENDPOINT = "https://calm-spire-77666.herokuapp.com/api";
 
 export const fetchUser = async (userslug) => await axios.get(`${APIENDPOINT}/users/${userslug}`);
