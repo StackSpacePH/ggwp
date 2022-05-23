7074f332690f5005bda34462c57ec064f5cda8ca
Nadie <nadiefiind@gmail.com>
2022-05-23 18:28:53 +0800

    Change default Express port
    The default Express.js port is conflicting with Next.js port. (3000)

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import pages from './routes/pages.routes.js';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use(cors())
 app.use(express.json())
