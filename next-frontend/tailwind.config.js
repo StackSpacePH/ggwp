44034e34021c400d4b274f86e7a2783bed8e4efd
Jabez Borja <jabez.natsu@gmail.com>
2022-05-23 14:16:20 +0800

    [next] Test Fix: TailwindCSS style error

--- next-frontend/tailwind.config.js
+++ next-frontend/tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   content: [
     './pages/*.{js,ts,jsx,tsx}',
+    './pages/index.js',
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
-  darkMode: false,
+  darkMode: 'class',
   theme: {
     extend: {},
   },
