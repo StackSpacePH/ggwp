2de8772614618cfecf3e328beb4c20d6a9f01682
Jabez Borja <jabez.natsu@gmail.com>
2022-05-24 13:32:11 +0800

    [next] Change: devDeps to deps

--- next-frontend/tailwind.config.js
+++ next-frontend/tailwind.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   content: [
-    './pages/*.{js,ts,jsx,tsx}',
-    './pages/index.js',
     './pages/**/*.{js,ts,jsx,tsx}',
-    './components/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: 'class',
